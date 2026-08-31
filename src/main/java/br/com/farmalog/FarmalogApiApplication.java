5f4b61ae9e4a61721dff06cec42194735ef99ea9
ggabmartins <gabriellm1805@gmail.com>
2026-08-31 19:37:02 -0300

    chore: estrutura inicial do projeto com spring boot 4.1

---
+++ src/main/java/br/com/farmalog/FarmalogApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.farmalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmalogApiApplication.class, args);
+	}
+
+}
